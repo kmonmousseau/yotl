6772a31403374def818868095a9467e7ddac1cc3
kmonmousseau <k.monmousseau@gmail.com>
2020-01-15 20:57:32 +0100

    add webpack encore and inital config

---
+++ assets/js/admin.js
@@ -0,0 +1,24 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../css/admin.css';
+
+import 'admin-lte/plugins/fontawesome-free/css/all.min.css'
+import 'admin-lte/dist/css/adminlte.min.css'
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+// Bootstrap 4
+import bootstrap from 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+// AdminLTE App
+import adminlte from 'admin-lte/dist/js/adminlte.min.js';
+
+console.log('Hello Webpack Encore! Edit me in assets/js/admin.js');
